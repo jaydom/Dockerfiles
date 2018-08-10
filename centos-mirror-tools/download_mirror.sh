943aa0a0c23f55d01b14d686b09c5f84f22e4a77
jaydomchen <jaydom912@126.com>
2018-08-10 14:30:48 +0800

    add starlingx mirror  Changes to be committed: 	modified:   Dockerfile 	new file:   README.rst 	new file:   dl_other_from_centos_repo.sh 	new file:   dl_rpms.sh 	new file:   dl_tarball.sh 	new file:   download_mirror.sh 	new file:   mvn-artifacts.lst 	new file:   other_downloads.lst 	new file:   rpm-gpg-keys/RPM-GPG-KEY-CentOS-5 	new file:   rpm-gpg-keys/RPM-GPG-KEY-CentOS-6 	new file:   rpm-gpg-keys/RPM-GPG-KEY-CentOS-Debug-6 	new file:   rpm-gpg-keys/RPM-GPG-KEY-CentOS-SIG-OpsTools 	new file:   rpm-gpg-keys/RPM-GPG-KEY-CentOS-SIG-SCLo 	new file:   rpm-gpg-keys/RPM-GPG-KEY-CentOS-Security-6 	new file:   rpm-gpg-keys/RPM-GPG-KEY-CentOS-Testing-6 	new file:   rpm-gpg-keys/RPM-GPG-KEY-Cloud-SIG 	new file:   rpm-gpg-keys/RPM-GPG-KEY-DOCKER 	new file:   rpm-gpg-keys/RPM-GPG-KEY-EPEL-6 	new file:   rpm-gpg-keys/RPM-GPG-KEY-EPEL-7 	new file:   rpm-gpg-keys/RPM-GPG-KEY-FC27 	new file:   rpm-gpg-keys/RPM-GPG-KEY-FC27-primary 	new file:   rpm-gpg-keys/RPM-GPG-KEY-FC27-second 	new file:   rpm-gpg-keys/RPM-GPG-KEY-FC28 	new file:   rpm-gpg-keys/RPM-GPG-KEY-FC29 	new file:   rpm-gpg-keys/RPM-GPG-KEY-cern 	new file:   rpms_from_3rd_parties.lst 	new file:   rpms_from_centos_3rd_parties.lst 	new file:   rpms_from_centos_repo.lst 	new file:   show_imported_rpm_gpg_keys.sh 	new file:   starlingx_add_pkgs.sh 	new file:   tarball-dl.lst 	new file:   yum.repos.d/StarlingX-Centos-7.2.repo 	new file:   yum.repos.d/StarlingX-Centos-7.3.repo 	new file:   yum.repos.d/StarlingX-Centos-7.4.repo 	new file:   yum.repos.d/StarlingX-Centos-7.5.repo 	new file:   yum.repos.d/StarlingX-EPEL-7.repo 	new file:   yum.repos.d/StarlingX_3rd_buildlogs-seed_centos_org.repo 	new file:   yum.repos.d/StarlingX_3rd_buildlogs_centos_org.repo 	new file:   yum.repos.d/StarlingX_3rd_download_docker_com.repo 	new file:   yum.repos.d/StarlingX_3rd_epel_blizoo_mk.repo 	new file:   yum.repos.d/StarlingX_3rd_ftp_iij_ad_jp.repo 	new file:   yum.repos.d/StarlingX_3rd_ftp_riken_jp.repo 	new file:   yum.repos.d/StarlingX_3rd_mirrors_isu_net_sa.repo 	new file:   yum.repos.d/StarlingX_3rd_www_rpmfind_net.repo

---
+++ centos-mirror-tools/download_mirror.sh
@@ -0,0 +1,129 @@
+#!/bin/bash -e
+echo "--------------------------------------------------------------"
+
+echo "WARNING: this script HAS TO access internet (http/https/ftp),"
+echo "so please make sure your network working properly!!"
+
+mkdir -p ./logs
+
+need_file(){
+    for f in $*; do
+        if [ ! -e $f ]; then
+            echo "ERROR: $f does not exist."
+            exit -1
+        fi
+    done
+}
+
+# Check extistence of prerequisites files
+need_file dl_rpms.sh dl_other_from_centos_repo.sh tarball-dl.sh
+need_file rpms_from_3rd_parties.lst
+need_file rpms_from_centos_3rd_parties.lst
+need_file rpms_from_centos_repo.lst
+need_file other_downloads.lst
+need_file tarball-dl.lst mvn-artifacts.lst
+
+#download RPMs/SRPMs from 3rd_party websites (not CentOS repos) by "wget"
+echo "step #1: start downloading RPMs/SRPMs from 3rd-party websites..."
+
+# Restore StarlingX_3rd repos from backup
+REPO_SOURCE_DIR=/localdisk/yum.repos.d
+REPO_DIR=/etc/yum.repos.d
+if [ -d $REPO_SOURCE_DIR ] && [ -d $REPO_DIR ]; then
+    \cp -f $REPO_SOURCE_DIR/*.repo $REPO_DIR/
+fi
+
+rpm_downloader="./dl_rpms.sh"
+$rpm_downloader ./rpms_from_3rd_parties.lst L1 3rd | tee ./logs/log_download_rpms_from_3rd_party.txt
+if [ $? != 0 ];then
+    echo "ERROR: something wrong with downloading, please check the log!!"
+fi
+
+# download RPMs/SRPMs from 3rd_party repos by "yumdownloader"
+$rpm_downloader ./rpms_from_centos_3rd_parties.lst L1 3rd-centos | tee ./logs/log_download_rpms_from_centos_3rd_parties_L1.txt
+
+# deleting the StarlingX_3rd to avoid pull centos packages from the 3rd Repo.
+\rm -f $REPO_DIR/StarlingX_3rd*.repo
+
+echo "step #2: start 1st round of downloading RPMs and SRPMs with L1 match criteria..."
+#download RPMs/SRPMs from CentOS repos by "yumdownloader"
+
+$rpm_downloader ./rpms_from_centos_repo.lst L1 centos | tee ./logs/log_download_rpms_from_centos_L1.txt
+
+if [ $? == 0 ]; then
+    echo "finish 1st round of RPM downloading successfully!"
+    if [ -e "./output/centos_rpms_missing_L1.txt" ]; then
+        missing_num=`wc -l ./output/centos_rpms_missing_L1.txt | cut -d " " -f1-1`
+        if [ "$missing_num" != "0" ];then
+            echo "ERROR:  -------RPMs missing $missing_num in yumdownloader with L1 match ---------------"
+        fi
+        #echo "start 2nd round of downloading Binary RPMs with L2 match criteria..."
+        #$rpm_downloader ./output/centos_rpms_missing_L1.txt L2 centos | tee ./logs/log_download_rpms_from_centos_L2.txt
+        #if [ $? == 0 ]; then
+        #    echo "finish 2nd round of RPM downloading successfully!"
+        #    if [ -e "./output/rpms_missing_L2.txt" ]; then
+        #        echo "WARNING: we're still missing RPMs listed in ./rpms_missing_L2.txt !"
+        #    fi
+        #fi
+    fi
+
+    if [ -e "./output/centos_srpms_missing_L1.txt" ]; then
+        missing_num=`wc -l ./output/centos_srpms_missing_L1.txt | cut -d " " -f1-1`
+        if [ "$missing_num" != "0" ];then
+            echo "ERROR: --------- SRPMs missing $missing_num in yumdownloader with L1 match ---------------"
+        fi
+        #echo "start 2nd round of downloading Source RPMs with L2 match criteria..."
+        #$rpm_downloader ./output/centos_srpms_missing_L1.txt L2 centos | tee ./logs/log_download_srpms_from_centos_L2.txt
+        #if [ $? == 0 ]; then
+        #    echo "finish 2nd round of SRPM downloading successfully!"
+        #    if [ -e "./srpms_missing_L2.txt" ]; then
+        #        echo "WARNING: we're still missing SRPMs listed in ./rpms_missing_L2.txt !"
+        #    fi
+        #fi
+    fi
+else
+    echo "finish 1st round with failures!"
+fi
+
+## verify all RPMs SRPMs we download for the GPG keys
+find ./output -type f -name "*.rpm" | xargs rpm -K | grep -i "MISSING KEYS" > ./rpm-gpg-key-missing.txt
+
+# remove all i686.rpms to avoid pollute the chroot dep chain
+find ./output -name "*.i686.rpm" | tee ./output/all_i686.txt
+find ./output -name "*.i686.rpm" | xargs rm -f
+
+line1=`wc -l rpms_from_3rd_parties.lst | cut -d " " -f1-1`
+line2=`wc -l rpms_from_centos_repo.lst | cut -d " " -f1-1`
+line3=`wc -l rpms_from_centos_3rd_parties.lst | cut -d " " -f1-1`
+let total_line=$line1+$line2+$line3
+echo "We expect to download $total_line RPMs."
+num_of_downloaded_rpms=`find ./output -type f -name "*.rpm" | wc -l | cut -d" " -f1-1`
+echo "There are $num_of_downloaded_rpms RPMs in output directory."
+if [ "$total_line" != "$num_of_downloaded_rpms" ]; then
+    echo "WARNING: Not the same number of RPMs in output as RPMs expected to be downloaded, need to check outputs and logs."
+fi
+
+# change "./output" and sub-folders to 751 (cgcs) group
+chown  751:751 -R ./output
+
+
+echo "step #3: start downloading other files ..."
+
+other_downloader="./dl_other_from_centos_repo.sh"
+$other_downloader ./other_downloads.lst ./output/stx-r1/CentOS/pike/Binary/ | tee ./logs/log_download_other_files_centos.txt
+if [ $? == 0 ];then
+    echo "step #3: done successfully"
+fi
+
+# StarlingX requires a group of source code pakages, in this section
+# they will be downloaded.
+echo "step #4: start downloading tarball compressed files"
+./dl_tarball.sh
+
+echo "IMPORTANT: The following 3 files are just bootstrap versions. Based"
+echo "on them, the workable images for StarlingX could be generated by"
+echo "running \"update-pxe-network-installer\" command after \"build-iso\""
+echo "    - out/stx-r1/CentOS/pike/Binary/LiveOS/squashfs.img"
+echo "    - out/stx-r1/CentOS/pike/Binary/images/pxeboot/initrd.img"
+echo "    - out/stx-r1/CentOS/pike/Binary/images/pxeboot/vmlinuz"
+
